2e0909d865eab83d42b08b907bf079eeeb309750
Pwera <piotr.wera@vp.pl>
2018-06-11 20:39:56 +0200

    new module: fmem

--- libs/CMakeezer.sh
+++ libs/CMakeezer.sh
@@ -13,7 +13,8 @@ echo    Ninja
 echo    Unix Makefiles
 
 echo -e
-read UserGenerator
+#read UserGenerator
+UserGenerator=Ninja
 
 echo $UserGenerator
 UserGenerator2=$"${UserGenerator// /_}"
@@ -25,8 +26,8 @@ echo -e
 echo -e
 echo Set Your repository. Linux Platform:
 echo  /home/user/repo
-read repositoryDir
-
+#read repositoryDir
+repositoryDir=/home/user/repo
 InstallationPrefix=${repositoryDir}/${UserGeneratorSubstring}Build
 echo $InstallationPrefix
 
@@ -38,7 +39,7 @@ echo  $mypath
 echo -e
 echo -e
 
-Cmakeezer_Options="-DCMAKE_INSTALL_PREFIX=${InstallationPrefix} -DCMAKE_MODULE_PATH=$InstallationPrefix -DCMAKE_PREFIX_PATH=$InstallationPrefix -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+Cmakeezer_Options="-DCMAKE_INSTALL_PREFIX=${InstallationPrefix} -DCMAKE_MODULE_PATH=$InstallationPrefix;${mypath}/cmake-findgmock -DCMAKE_PREFIX_PATH=$InstallationPrefix -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
 
 echo ==========================================================================================
 echo ==========================================================================================
@@ -62,260 +63,117 @@ else
 fi
 
 
-cd googletest
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
+addDependency () {
+	echo Inside function $1 $2
+	cd $mypath
+	cd $1
+	mkdir $CmakeezerBuild
+	cd $CmakeezerBuild
+	cmake -G"$UserGenerator"  $Cmakeezer_Options $2 ..
+	cmake --build . $generateDefaultOptions
+	#cmake --build . --target memcheck
+	cmake --build . --target install
+	cd $mypaths
+	echo @@@@@@@@@ $mypath @@@@@@@@@
+	echo -e
+	echo -e
+}
+
+# TODO: FINISH ME
+#addDependency "czmq" "-DENABLE_DRAFTS=OFF -DCZMQ_BUILD_STATIC=ON -DCZMQ_BUILD_SHARED=OFF"
+#addDependency "immutable-cpp"
+#addDependency "ui"
+#addDependency "eraserface"
+#addDependency "smartref"
+#addDependency "nanogui"
+#addDependency "impala"
+#addDependency "CompilerExplorer"
+#addDependency "futures_cpp" "-DENABLE_TEST=OFF -DENABLE_EXAMPLES=OFF"
+#addDependency "openthread"
+#addDependency "easycond"
+#addDependency "vireo"
+#addDependency "petra"
+#addDependency "virtual-concepts"
+#addDependency "coroutine_monad"
+#addDependency "qpid-cpp"
+#addDependency "copperspice"
+#addDependency "cpp-dependencies"
+#addDependency "Kvasir"
+#addDependency "easyLambda"
+#addDependency "envoy"
+#addDependency "crow" #require boost
+#addDependency "Synca" #require boost
+#addDependency "taichi"
+#addDependency "yue"
+#addDependency "dpl"
+#addDependency "eos" ##require Eigen
+#addDependency "libraries" #require boost
+#addDependency "boringssl" #require go
+#addDependency "themis"  #require boring-ssl
+#addDependency "libtins" #require PCAP
+#addDependency "redox"  #require hiredis
+#addDependency "ctti" "-DCTTI_BUILD_TESTS=ON -DCTTI_BUILD_EXAMPLES=ON" #build error
+#addDependency "treefrog-framework" #does not appear to contain CMakeLists.txt
+#addDependency "metaverse" #require miniupnpc
+addDependency "gherkin-c"
+addDependency "fmem"
+addDependency "gherkin-cpp"
+addDependency "GUnit"
+exit 1
+#git submodule update --init --recursive
+#addDependency "rocksdb" "-DWITH_TESTS=OFF -DWITH_TOOLS=OFF -DWITH_JEMALLOC=OFF -DWITH_SNAPPY=OFF -DWITH_LZ4=OFF -DWITH_ZLIB=OFF"
+addDependency "googletest"
+addDependency "CLUE" "-DBUILD_TESTS=OFF"
+addDependency "libsolace"
+addDependency "dynamix"
+addDependency "Fido"
+addDependency "lambda"
+addDependency "hippomocks"
+addDependency "boson"
+addDependency "SkyPat"
+addDependency "jubatus"
+addDependency "rr"
+addDependency "metal"
+addDependency "syscpp"
+addDependency "sqlite_orm"
+addDependency "taichi"
+addDependency "lager"
+addDependency "asyncplusplus"
+addDependency "Catch2" "-DCATCH_ENABLE_WERROR=OFF -DNO_SELFTEST=YES"
+addDependency "GSL"
+addDependency "rapidjson" "-DRAPIDJSON_BUILD_TESTS=OFF -DRAPIDJSON_BUILD_DOC=OFF"
+addDependency "libzmq" "-DWITH_DOC=OFF -DBUILD_TESTS=OFF"
+addDependency "CppMicroServices"
+addDependency "cmcstl2" "-DENABLE_TESTING=FALSE"
+addDependency "hof"
+addDependency "FakeIt"
+addDependency "immer"
+addDependency "hana"
+addDependency "function2"
+addDependency "continuable" "-DCTI_CONTINUABLE_WITH_EXAMPLES=OFF -DCTI_CONTINUABLE_WITH_TESTS=OFF"
+addDependency "benchmark" "-DBENCHMARK_ENABLE_TESTING=OFF"
+addDependency "backward-cpp" "-DSTACK_WALKING_UNWIND:BOOL=FALSE"
+addDependency "curl" "-DBUILD_TESTING=OFF"
+addDependency "SFML" "-DSFML_BUILD_DOC=OFF"
+addDependency "tiny-dnn"
+addDependency "brigand"
+addDependency "future"
+addDependency "trompeloeil"
+addDependency "alloy"
+addDependency "valuable"
+addDependency "cat"
+addDependency "tacopie" "-DBUILD_EXAMPLES=ON -DBUILD_TESTS=ON"
+addDependency "cpp_redis" "-DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF"
+addDependency "callable_traits"
+addDependency "dyno"
+addDependency "variant"
+addDependency "range-v3" "-DRANGE_V3_TESTS=OFF"
+addDependency "te" "-DENABLE_SANITIZERS=ON -DENABLE_TESTS=ON"
+addDependency "bullet3" "-DBUILD_CPU_DEMOS=ON"
+addDependency "metal"
+
+
 cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-echo current: $PWD
-
-cd CLUE
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DBUILD_TESTS=OFF ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd asyncplusplus
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-
-cd Catch2
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DCATCH_ENABLE_WERROR=OFF -DNO_SELFTEST=YES ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd GSL
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd rapidjson
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DRAPIDJSON_BUILD_TESTS=OFF -DRAPIDJSON_BUILD_DOC=OFF ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-
-cd libzmq
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DWITH_DOC=OFF -DBUILD_TESTS=OFF ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd CppMicroServices
-mkdir $CmakeezerBuild$
-cd $CmakeezerBuild$
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd cmcstl2
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd hof
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd FakeIt
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd immer
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd hana
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd function2
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options  ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd continuable
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DCTI_CONTINUABLE_WITH_EXAMPLES=OFF -DCTI_CONTINUABLE_WITH_TESTS=OFF ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-cd benchmark
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DBENCHMARK_ENABLE_TESTING=OFF ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd backward-cpp
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DSTACK_WALKING_UNWIND:BOOL=FALSE ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-
-cd curl
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DBUILD_TESTING=OFF ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd SFML
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options -DSFML_BUILD_DOC=OFF ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd tiny-dnn
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
-
-cd brigand
-mkdir $CmakeezerBuild
-cd $CmakeezerBuild
-cmake -G"$UserGenerator"  $Cmakeezer_Options ..
-cmake --build . $generateDefaultOptions
-cmake --build . --target install
-cd $mypath
-echo @@@@@@@@@ $mypath @@@@@@@@@
-echo -e
-echo -e
-
 cd YourProjectStartHere
 mkdir $CmakeezerBuild
 cd $CmakeezerBuild
@@ -346,3 +204,6 @@ echo ==============   -Add optional generator option,
 echo ==============   -Add precompiled header support
 echo ==============   -Add code coverage 
 echo ==============   -Add option to enable/disable Unit tests
+
+
+
